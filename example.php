ac894e2c5a211614fadcc7d667fa8d9bd9ef0067
hcharbonnier <hugues@XPS15.localdomain>
2020-03-20 13:54:13 +0100

    fix

--- example.php
+++ example.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer autoload
 
-use mangatranslation\MangaImage;
-use mangatranslation\TextBlock;
+use hcharbonnier\mangatranslation\MangaImage;
+use hcharbonnier\mangatranslation\TextBlock;
 
 $test = new MangaImage($argv[1]);
 $test->load();
